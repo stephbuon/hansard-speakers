5012f6f8bf533806ef634350266cc0981f763fd7
alexanderr <alexanderr@users.noreply.github.com>
2021-01-05 12:14:45 -0600

    utilize new data struct, count office matches as hits for now

--- hansard/worker.py
+++ hansard/worker.py
@@ -1,22 +1,20 @@
-from datetime import datetime
 import multiprocessing
 from queue import Empty
-import re
-from .speaker import SpeakerReplacement, OfficeHolding
-from typing import Dict, List
-import pandas as pd
+from hansard.loader import DataStruct
 
 
 # This function will run per core.
 def worker_function(inq: multiprocessing.Queue,
                     outq: multiprocessing.Queue,
-                    holdings: List[OfficeHolding],
-                    full_alias_dict: Dict[str, List[SpeakerReplacement]],
-                    misspellings_dict: Dict[str, str],
-                    exchaequer_df: pd.DataFrame,
-                    pm_df: pd.DataFrame,
-                    terms_df: pd.DataFrame):
+                    data: DataStruct):
     from . import cleanse_string
+
+    misspellings_dict = data.corrections
+    exchaequer_df = data.exchequer_df
+    pm_df = data.pm_df
+    holdings = data.holdings
+    alias_dict = data.alias_dict
+    terms_df = data.term_df
 
     while True:
         try:
@@ -25,7 +23,7 @@ def worker_function(inq: multiprocessing.Queue,
             continue
         else:
             match = None
-            ambiguity = False
+            ambiguity: bool = False
             target = cleanse_string(target)
             possibles = []
             query = None
@@ -36,23 +34,33 @@ def worker_function(inq: multiprocessing.Queue,
 
             target = cleanse_string(target)
 
+            # Don't count these as ambigious for now
+            # TODO: need to add MP id to the CSV's.
             if 'exchequer' in target:
                 query = exchaequer_df[(speechdate >= exchaequer_df['started_service']) & (speechdate < exchaequer_df['ended_service'])]
-                if len(query) >= 1:
+                if len(query) == 1:
                     target = query.iloc[0]['real_name'].lower()
+                    match = target
+                elif len(query) > 1:
+                    ambiguity = True
+
             elif 'prime minister' in target:
                 query = pm_df[(speechdate >= pm_df['started_service']) & (speechdate < pm_df['ended_service'])]
-                if len(query) >= 1:
+                if len(query) == 1:
                     target = query.iloc[0]['real_name'].lower()
+                    match = target
+                elif len(query) > 1:
+                    ambiguity = True
 
             # can we get ambiguities with office names?
-            for holding in holdings:
-                if holding.matches(target, speechdate, cleanse=False):
-                    match = holding
-                    break
+            if not match:
+                for holding in holdings:
+                    if holding.matches(target, speechdate, cleanse=False):
+                        match = holding
+                        break
 
             if not match:
-                possibles = full_alias_dict.get(target)
+                possibles = alias_dict.get(target)
                 if possibles is not None:
                     possibles = [speaker for speaker in possibles if speaker.matches(target, speechdate, cleanse=False)]
                     if len(possibles) == 1:
