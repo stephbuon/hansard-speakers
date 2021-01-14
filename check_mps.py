09e76d00750efd17f39709d4e159dcfc38cbf81e
alexanderr <alexanderr@users.noreply.github.com>
2021-01-13 21:23:18 -0600

    additional data validation

--- check_mps.py
+++ check_mps.py
@@ -19,9 +19,13 @@ def is_malformed(row):
         return 'PARENTHESIS'
 
     if firstname not in nameparts:
+        if ' ' in firstname:
+            return 'SPACE_IN_FIRSTNAME'
         return 'MISSING_FIRSTNAME'
 
     if surname not in nameparts:
+        if ' ' in surname:
+            return 'SPACE_IN_SURNAME'
         return 'MISSING_SURNAME'
 
     if nameparts.index(firstname) > 1:
@@ -40,3 +44,49 @@ mps['malformed'] = mps.apply(is_malformed, axis=1)
 malformed_df = mps[~mps['malformed'].isnull()]
 malformed_df.to_csv('malformed_mps.csv', sep=',')
 print(f'{len(malformed_df)} rows with malformed information.')
+
+
+from hansard import DATE_FORMAT2
+
+
+exchequer_df = pd.read_csv('data/chancellor_of_the_exchequer.csv', sep=',')
+try:
+    exchequer_df['started_service'] = pd.to_datetime(exchequer_df['started_service'], format=DATE_FORMAT2)
+    exchequer_df['ended_service'] = pd.to_datetime(exchequer_df['ended_service'], format=DATE_FORMAT2)
+except ValueError as e:
+    print('Exchequer dataset has invalid dates.')
+    raise e
+finally:
+    invalid_entries = exchequer_df[~exchequer_df['corresponding_id'].isin(mps.index)]
+    if len(invalid_entries):
+        print('Exchequer dataset has invalid ids.')
+        for v in invalid_entries.itertuples():
+            print(v)
+
+pm_df = pd.read_csv('data/prime_ministers.csv', sep=',')
+try:
+    pm_df['started_service'] = pd.to_datetime(pm_df['started_service'], format=DATE_FORMAT2)
+    pm_df['ended_service'] = pd.to_datetime(pm_df['ended_service'], format=DATE_FORMAT2)
+except ValueError as e:
+    print('prime minister dataset has invalid dates.')
+    raise e
+finally:
+    invalid_entries = pm_df[~pm_df['corresponding_id'].isin(mps.index)]
+    if len(invalid_entries):
+        print('prime minister dataset has invalid ids.')
+        for v in invalid_entries.itertuples():
+            print(v)
+
+try:
+    lord_chance_df = pd.read_csv('data/lord_chancellors.csv', sep=',')
+    lord_chance_df['started_service'] = pd.to_datetime(lord_chance_df['started_service'], format=DATE_FORMAT2)
+    lord_chance_df['ended_service'] = pd.to_datetime(lord_chance_df['ended_service'], format=DATE_FORMAT2)
+except ValueError as e:
+    print('lord chancellor dataset has invalid dates.')
+    raise e
+finally:
+    invalid_entries = lord_chance_df[~lord_chance_df['corresponding_id'].isin(mps.index)]
+    if len(invalid_entries):
+        print('lord chancellor dataset has invalid ids.')
+        for v in invalid_entries.itertuples():
+            print(v)
