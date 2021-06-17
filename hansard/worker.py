4ef522dd4b745ac6467693678520abc1a72d27a5
Alex <localtoasted@pm.me>
2021-06-17 17:50:04 -0500

    push additional name aliases

--- hansard/worker.py
+++ hansard/worker.py
@@ -137,7 +137,7 @@ REGEX_POST_CORRECTIONS = [
     ('^air +', 'sir '),
     ('^si +', 'sir '),
     ('^sdi +', 'sir '),
-    ('^slr +', 'sir ')
+    ('^slr +', 'sir '),
     
     ('^abmiral +', 'admiral '),
     ('^admtral +', 'admiral '),
