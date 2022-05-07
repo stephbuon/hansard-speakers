8523bdcf3096b563499eb4077c5e7a827c7667f8
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-07 13:06:51 -0500

    Update top_speakers_before_and_after.R

--- evaluation/top_speakers_before_and_after.R
+++ evaluation/top_speakers_before_and_after.R
@@ -41,3 +41,40 @@ top_speakers <- words_per_day %>%
   filter(sp_ranking_decade <= 5)
 
 write_csv(top_speakers, "top_new_speakers_eval.csv")
+
+
+library(tidyverse)
+library(data.table)
+library(plotly)
+library(ggrepel)
+library(scales)
+
+top_speakers <- fread("/home/stephbuon/projects/hansard-speakers/evaluation-data/top_speakers_eval.csv")
+
+top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_day)
+
+# it's actually words per decade
+ggplot(top_speakers, 
+       aes(decade, 
+           words_per_day, 
+           label = label)) +
+  geom_text_repel(force = 1) +
+  geom_point(color = "#4682b4") +
+  theme_classic(base_size = 16) + 
+  scale_y_continuous(labels = comma)
+
+
+
+top_new_speakers <- fread("/home/stephbuon/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
+
+top_new_speakers$label <- paste0(top_new_speakers$speaker, ", ", top_new_speakers$words_per_day)
+
+
+ggplot(top_new_speakers, 
+       aes(decade, 
+           words_per_day, 
+           label = label)) +
+  geom_text_repel(force = 1) +
+  geom_point(color = "#4682b4") +
+  theme_classic(base_size = 16) + 
+  scale_y_continuous(labels = comma)
