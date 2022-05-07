db244f7a45f493e13924e43388d6eb67199dbdc1
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-07 12:40:17 -0500

    Create top_speakers_before_and_after.R

---
+++ evaluation/top_speakers_before_and_after.R
@@ -0,0 +1,38 @@
+library(data.table)
+library(tidytext)
+library(tidyverse)
+
+hansard <- fread("/scratch/group/pract-txt-mine/hansard_c19_improved_speaker_names_2.csv") %>%
+  select(speaker, new_speaker, text, speechdate) %>%
+  mutate(year = year(speechdate)) %>%
+  mutate(decade = year - year %% 10) %>%
+  select(-year, -speechdate)
+
+tokenized_hansard <- hansard %>%
+  unnest_tokens(word, text)
+
+words_per_day <- tokenized_hansard %>%
+  group_by(decade, speaker) %>%
+  summarize(words_per_day = n())
+
+words_per_day <- words_per_day %>%
+  group_by(decade) %>%
+  mutate(sp_ranking_decade = rank(desc(words_per_day)))
+
+top_speakers <- words_per_day %>%
+  filter(sp_ranking_decade <= 5)
+
+write_csv(top_speakers, "top_speakers_eval.csv")
+
+words_per_day <- tokenized_hansard %>%
+  group_by(decade, new_speaker) %>%
+  summarize(words_per_day = n())
+
+words_per_day <- words_per_day %>%
+  group_by(decade) %>%
+  mutate(sp_ranking_decade = rank(desc(words_per_day)))
+
+top_speakers <- words_per_day %>%
+  filter(sp_ranking_decade <= 5)
+
+write_csv(top_speakers, "top_new_speakers_eval.csv")
