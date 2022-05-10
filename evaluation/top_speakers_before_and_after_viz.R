dfecdd138f650ea96e715e0d831cc563ddaf61c3
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-10 15:02:38 -0500

    Update top_speakers_before_and_after_viz.R

--- evaluation/top_speakers_before_and_after_viz.R
+++ evaluation/top_speakers_before_and_after_viz.R
@@ -8,10 +8,14 @@ top_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_speakers_
 
 top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_decade)
 
+# it's actually words per decade
 ggplot(top_speakers,
        aes(decade,
            words_per_decade,
            label = label)) +
+  ylab("Word Count Per Decade") +
+  xlab("Decade") +
+  ggtitle("Top Speakers Per Decade and Their Word Counts: Original Data") +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
@@ -21,10 +25,17 @@ top_new_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_new_s
 
 top_new_speakers$label <- paste0(top_new_speakers$new_speaker, ", ", top_new_speakers$words_per_decade)
 
+
+top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", " ")
+top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "[:digit:]+", " ")
+
 ggplot(top_new_speakers,
        aes(decade,
            words_per_decade,
            label = label)) +
+  ylab("Word Count Per Decade") +
+  xlab("Decade") +
+  ggtitle("Top Speakers Per Decade and Their Word Counts: With Disambiguated Speakers") +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
