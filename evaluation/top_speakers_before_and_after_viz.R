0ace60237f1a59374869cfa3e59480d7689fd00c
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-10 14:55:56 -0500

    Update top_speakers_before_and_after_viz.R

--- evaluation/top_speakers_before_and_after_viz.R
+++ evaluation/top_speakers_before_and_after_viz.R
@@ -4,44 +4,28 @@ library(plotly)
 library(ggrepel)
 library(scales)
 
-top_speakers <- fread("/home/steph/Downloads/top_speakers_eval.csv")
+top_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_speakers_eval.csv")
 
-top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_day)
+top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_decade)
 
-# it's actually words per decade
 ggplot(top_speakers,
        aes(decade,
-           words_per_day,
+           words_per_decade,
            label = label)) +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
   scale_y_continuous(labels = comma)
 
+top_new_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
 
-
-top_new_speakers <- fread("/home/steph/Downloads/top_new_speakers_eval.csv")
-
-top_new_speakers$label <- paste0(top_new_speakers$speaker, ", ", top_new_speakers$words_per_day)
-
+top_new_speakers$label <- paste0(top_new_speakers$new_speaker, ", ", top_new_speakers$words_per_decade)
 
 ggplot(top_new_speakers,
        aes(decade,
-           words_per_day,
+           words_per_decade,
            label = label)) +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
-  scale_y_continuous(labels = comma)
-
-top_new_speakers$label <- paste0(top_new_speakers$speaker, ", ", top_new_speakers$words_per_day)
-
-
-ggplot(top_new_speakers, 
-       aes(decade, 
-           words_per_day, 
-           label = label)) +
-  geom_text_repel(force = 1) +
-  geom_point(color = "#4682b4") +
-  theme_classic(base_size = 16) + 
   scale_y_continuous(labels = comma)
