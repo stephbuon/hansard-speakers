998eea69be46d450a2be7ef00c370dca8947d13d
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-10 13:05:14 -0500

    Update top_speakers_before_and_after.R

--- evaluation/top_speakers_before_and_after.R
+++ evaluation/top_speakers_before_and_after.R
@@ -2,32 +2,30 @@ library(data.table)
 library(tidytext)
 library(tidyverse)
 
-hansard <- fread("/scratch/group/pract-txt-mine/hansard_c19_improved_speaker_names_2.csv") %>%
+hansard <- fread("/home/steph/Downloads/hansard_c19_improved_speaker_names_2.csv") %>%
   select(speaker, new_speaker, text, speechdate) %>%
   mutate(year = year(speechdate)) %>%
   mutate(decade = year - year %% 10) %>%
   select(-year, -speechdate)
 
-tokenized_hansard <- hansard %>%
+hansard <- hansard %>%
   unnest_tokens(word, text)
 
-
-
-words_per_day <- tokenized_hansard %>%
+words_per_day <- hansard %>%
   group_by(decade, speaker) %>%
   summarize(words_per_decade = n())
 
 words_per_day <- words_per_day %>%
   group_by(decade) %>%
-  mutate(sp_ranking_decade = rank(desc(words_per_day)))
+  mutate(sp_ranking_decade = rank(desc(words_per_decade)))
 
 top_speakers <- words_per_day %>%
   filter(sp_ranking_decade <= 5)
 
-write_csv(top_speakers, "top_speakers_eval.csv")
+write_csv(top_speakers, "/home/steph/Downloads/top_speakers_eval.csv")
 
-tokenized_hansard_2 <- tokenized_hansard %>%
-filter(!str_detect(new_speaker, "^NA$")) 
+hansard <- hansard %>%
+  filter(!str_detect(new_speaker, "^$"))
 
 words_per_day <- tokenized_hansard_2 %>%
   group_by(decade, new_speaker) %>%
@@ -40,7 +38,7 @@ words_per_day <- words_per_day %>%
 top_speakers <- words_per_day %>%
   filter(sp_ranking_decade <= 5)
 
-write_csv(top_speakers, "top_new_speakers_eval.csv")
+write_csv(top_speakers, "/home/steph/Downloads/top_new_speakers_eval.csv")
 
 
 library(tidyverse)
@@ -49,23 +47,35 @@ library(plotly)
 library(ggrepel)
 library(scales)
 
-top_speakers <- fread("/home/stephbuon/projects/hansard-speakers/evaluation-data/top_speakers_eval.csv")
+top_speakers <- fread("/home/steph/Downloads/top_speakers_eval.csv")
 
 top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_day)
 
 # it's actually words per decade
-ggplot(top_speakers, 
-       aes(decade, 
-           words_per_day, 
+ggplot(top_speakers,
+       aes(decade,
+           words_per_day,
            label = label)) +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
-  theme_classic(base_size = 16) + 
+  theme_classic(base_size = 16) +
   scale_y_continuous(labels = comma)
 
 
 
-top_new_speakers <- fread("/home/stephbuon/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
+top_new_speakers <- fread("/home/steph/Downloads/top_new_speakers_eval.csv")
+
+top_new_speakers$label <- paste0(top_new_speakers$speaker, ", ", top_new_speakers$words_per_day)
+
+
+ggplot(top_new_speakers,
+       aes(decade,
+           words_per_day,
+           label = label)) +
+  geom_text_repel(force = 1) +
+  geom_point(color = "#4682b4") +
+  theme_classic(base_size = 16) +
+  scale_y_continuous(labels = comma)
 
 top_new_speakers$label <- paste0(top_new_speakers$speaker, ", ", top_new_speakers$words_per_day)
 
