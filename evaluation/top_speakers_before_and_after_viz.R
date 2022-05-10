47815387ae2a98f66fdbf1bf9a28c90706725f85
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-10 15:07:57 -0500

    Update top_speakers_before_and_after_viz.R

--- evaluation/top_speakers_before_and_after_viz.R
+++ evaluation/top_speakers_before_and_after_viz.R
@@ -8,6 +8,8 @@ top_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_speakers_
 
 top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_decade)
 
+top_speakers$speaker = str_to_title(top_speakers$speaker)
+
 # it's actually words per decade
 ggplot(top_speakers,
        aes(decade,
@@ -21,6 +23,8 @@ ggplot(top_speakers,
   theme_classic(base_size = 16) +
   scale_y_continuous(labels = comma)
 
+
+
 top_new_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
 
 top_new_speakers$label <- paste0(top_new_speakers$new_speaker, ", ", top_new_speakers$words_per_decade)
@@ -28,6 +32,8 @@ top_new_speakers$label <- paste0(top_new_speakers$new_speaker, ", ", top_new_spe
 
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", " ")
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "[:digit:]+", " ")
+
+top_new_speakers$new_speaker = str_to_title(top_new_speakers$new_speaker)
 
 ggplot(top_new_speakers,
        aes(decade,
