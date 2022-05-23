66eeb4dda15d17a0a5dbf95a3ad459ba24c8aae2
Steph Buongiorno <52008049+stephbuon@users.noreply.github.com>
2022-05-23 14:26:40 -0500

    Update top_speakers_before_and_after_viz.R

--- evaluation/top_speakers_before_and_after_viz.R
+++ evaluation/top_speakers_before_and_after_viz.R
@@ -27,10 +27,10 @@ ggplot(top_speakers,
 
 top_new_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
 
-top_new_speakers$label <- paste0(top_new_speakers$new_speaker, ", ", top_new_speakers$words_per_decade)
-
+x_axis_labels <- c(1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890)
 
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", " ")
+top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", "")
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "[:digit:]+", " ")
 
 top_new_speakers$new_speaker = str_to_title(top_new_speakers$new_speaker)
@@ -38,11 +38,14 @@ top_new_speakers$new_speaker = str_to_title(top_new_speakers$new_speaker)
 ggplot(top_new_speakers,
        aes(decade,
            words_per_decade,
-           label = label)) +
+           label = paste0("Name: ", new_speaker, "\n",
+                          "Word Count: ", words_per_decade))) +
   ylab("Word Count Per Decade") +
   xlab("Decade") +
-  ggtitle("Top Speakers Per Decade and Their Word Counts: With Disambiguated Speakers") +
+  ggtitle("Top Speakers Per Decade and Their Word Counts",
+          subtitle = "With Disambiguated Speakers") +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
-  scale_y_continuous(labels = comma)
+  scale_y_continuous(labels = comma) + 
+  scale_x_discrete(labels = as.character(x_axis_labels), breaks = x_axis_labels)
