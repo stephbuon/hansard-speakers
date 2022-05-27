5484cf21b1cb7f70021eefc0a287aba1171b3c95
stephbuon <steph.buon@gmail.com>
2022-05-27 13:49:20 -0500

    updated speaker count side by side plot

--- evaluation/n_unique_speakers.R
+++ evaluation/n_unique_speakers.R
@@ -10,31 +10,52 @@ hansard <- fread("/home/stephbuon/data/hansard_c19_improved_speaker_names_2.csv"
   select(-year, -speechdate)
 
 disambig_speaker_count <- hansard %>%
-  select(decade, new_speaker) %>%
-  distinct() %>%
+  count(decade, new_speaker) %>%
+  select(-new_speaker) %>%
   group_by(decade) %>%
-  summarise(sum = n())
+  summarise(disambig_speaker = n()) 
 
-disambig_speaker_count %>%
-  plot_ly(x = ~decade, 
-          y = ~sum, 
-          type = 'bar') %>%
+original_speaker_count <- hansard %>%
+  count(decade, speaker) %>%
+  select(-speaker) %>%
+  group_by(decade) %>%
+  summarise(original_speaker = n()) 
+
+all_data <- left_join(original_speaker_count, disambig_speaker_count, by = "decade")
+
+plot_ly(data = all_data, 
+        x = ~decade, 
+        y = ~original_speaker, 
+        name = "Original Speakers",
+        type = 'bar',
+        text = ~original_speaker, textposition = 'top',
+        marker = list(color = 'rgb(49,130,189)')) %>%
+  add_trace(y = ~disambig_speaker,
+            name = "Disambiguated Speakers",
+            text = ~disambig_speaker, textposition = 'top',
+            marker = list(color = 'rgb(204,204,204)')) %>%
   layout(xaxis = list(title = "Decade", tickvals= ~decade, ticktext = ~decade),
          yaxis = list(title = "Number of Unique Speaker Names")) %>%
-  add_text(x = ~decade, y = ~sum , text = ~sum, textposition="top") %>%
   config(displayModeBar = F)
+  
 
-speaker_count <- hansard %>%
-  select(decade, speaker) %>%
-  distinct() %>%
-  group_by(decade) %>%
-  summarise(sum = n())
+# 
+# disambig_speaker_count %>%
+#   plot_ly(x = ~decade, 
+#           y = ~sum, 
+#           type = 'bar') %>%
+#   layout(xaxis = list(title = "Decade", tickvals= ~decade, ticktext = ~decade),
+#          yaxis = list(title = "Number of Unique Speaker Names")) %>%
+#   add_text(x = ~decade, y = ~sum , text = ~sum, textposition="top") %>%
+#   config(displayModeBar = F)
+# 
+# 
+# speaker_count %>%
+#   plot_ly(x = ~decade, 
+#           y = ~sum, 
+#           type = 'bar') %>%
+#   layout(xaxis = list(title = "Decade", tickvals= ~decade, ticktext = ~decade),
+#          yaxis = list(title = "Number of Unique Speaker Names")) %>%
+#   add_text(x = ~decade, y = ~sum , text = ~sum, textposition="top") %>%
+#   config(displayModeBar = F) 
 
-speaker_count %>%
-  plot_ly(x = ~decade, 
-          y = ~sum, 
-          type = 'bar') %>%
-  layout(xaxis = list(title = "Decade", tickvals= ~decade, ticktext = ~decade),
-         yaxis = list(title = "Number of Unique Speaker Names")) %>%
-  add_text(x = ~decade, y = ~sum , text = ~sum, textposition="top") %>%
-  config(displayModeBar = F) 
--- evaluation/top_speakers_before_and_after_viz.R
+++ evaluation/top_speakers_before_and_after_viz.R
@@ -6,7 +6,7 @@ library(scales)
 
 top_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_speakers_eval.csv")
 
-top_speakers$label <- paste0(top_speakers$speaker, ", ", top_speakers$words_per_decade)
+x_axis_labels <- c(1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890)
 
 top_speakers$speaker = str_to_title(top_speakers$speaker)
 
@@ -14,20 +14,21 @@ top_speakers$speaker = str_to_title(top_speakers$speaker)
 ggplot(top_speakers,
        aes(decade,
            words_per_decade,
-           label = label)) +
+           label = paste0("Name: ", speaker, "\n",
+                          "Word Count: ", words_per_decade))) +
   ylab("Word Count Per Decade") +
   xlab("Decade") +
-  ggtitle("Top Speakers Per Decade and Their Word Counts: Original Data") +
+  ggtitle("Top Speakers Per Decade and Their Word Counts",
+          subtitle = "Using the Original Hansard Speaker Field") +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
-  scale_y_continuous(labels = comma)
+  scale_y_continuous(labels = comma) + 
+  scale_x_continuous(breaks = top_speakers$decade)
 
 
 
 top_new_speakers <- fread("~/projects/hansard-speakers/evaluation-data/top_new_speakers_eval.csv")
-
-x_axis_labels <- c(1800, 1810, 1820, 1830, 1840, 1850, 1860, 1870, 1880, 1890)
 
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", " ")
 top_new_speakers$new_speaker <- str_replace(top_new_speakers$new_speaker, "_", "")
@@ -43,9 +44,10 @@ ggplot(top_new_speakers,
   ylab("Word Count Per Decade") +
   xlab("Decade") +
   ggtitle("Top Speakers Per Decade and Their Word Counts",
-          subtitle = "With Disambiguated Speakers") +
+          subtitle = "Using the Disambiguated Speaker Field") +
   geom_text_repel(force = 1) +
   geom_point(color = "#4682b4") +
   theme_classic(base_size = 16) +
   scale_y_continuous(labels = comma) + 
-  scale_x_discrete(labels = as.character(x_axis_labels), breaks = x_axis_labels)
+  scale_x_continuous(breaks = top_speakers$decade)
+
